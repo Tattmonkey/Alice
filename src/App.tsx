6e34054efaa8ba0ad5be197ae62caf323d96d00d
Tattmonkey <ngonimupandawana@gmail.com>
2024-12-19 17:55:32 +0200

    fix: add react-toastify and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './contexts/AuthContext';
 import { MessagingProvider } from './contexts/MessagingContext';
@@ -42,6 +42,8 @@ import ShippingManager from './components/admin/shipping/ShippingManager';
 import APISettings from './components/admin/APISettings';
 import ProtectedRoute from './components/ProtectedRoute';
 import AdminLogin from './pages/AdminLogin';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface AdminRouteProps {
   children: React.ReactNode;
@@ -102,197 +104,199 @@ export default function App() {
   };
 
   return (
-    <AuthProvider>
-      <GalleryProvider>
-        <BookingProvider>
-          <MessagingProvider>
-            <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-              <Navbar 
-                onOpenLoginModal={() => setIsLoginModalOpen(true)}
-                onOpenSignUpModal={() => setIsSignUpModalOpen(true)}
-              />
-              <Toaster position="top-center" />
-              <main className="flex-1">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/artists" element={<Artists />} />
-                  <Route path="/shop" element={<Shop />} />
-                  <Route path="/shop/product/:id" element={<ProductDetail />} />
-                  <Route path="/blog" element={<Blog />} />
-                  <Route path="/cart" element={<Cart />} />
-                  <Route path="/checkout" element={
-                    <ProtectedRoute>
-                      <Checkout />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/orders" element={
-                    <ProtectedRoute>
-                      <Orders />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/settings" element={
-                    <ProtectedRoute>
-                      <UserSettings />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/messages" element={
-                    <ProtectedRoute>
-                      <UserMessages />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/gallery" element={
-                    <ProtectedRoute>
-                      <UserGallery />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/bookings" element={
-                    <ProtectedRoute>
-                      <UserBookings />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/privacy" element={<PrivacyPolicy />} />
-                  <Route path="/terms" element={<TermsOfService />} />
-                  <Route path="/refunds" element={<RefundPolicy />} />
-                  <Route path="/admin" element={<AdminLogin />} />
-                  <Route
-                    path="/dashboard"
-                    element={
+    <Router>
+      <AuthProvider>
+        <GalleryProvider>
+          <BookingProvider>
+            <MessagingProvider>
+              <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+                <Navbar 
+                  onOpenLoginModal={() => setIsLoginModalOpen(true)}
+                  onOpenSignUpModal={() => setIsSignUpModalOpen(true)}
+                />
+                <Toaster position="top-center" />
+                <main className="flex-1">
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/artists" element={<Artists />} />
+                    <Route path="/shop" element={<Shop />} />
+                    <Route path="/shop/product/:id" element={<ProductDetail />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/checkout" element={
                       <ProtectedRoute>
-                        <UserDashboard />
+                        <Checkout />
                       </ProtectedRoute>
-                    }
-                  />
-                  <Route
-                    path="/artist/dashboard"
-                    element={
-                      <ArtistRoute>
-                        <ArtistDashboard />
-                      </ArtistRoute>
-                    }
-                  />
-                  <Route
-                    path="/artist/settings"
-                    element={
-                      <ArtistRoute>
-                        <ArtistSettings />
-                      </ArtistRoute>
-                    }
-                  />
-                  <Route
-                    path="/artist/setup"
-                    element={
-                      <ArtistRoute>
-                        <ArtistSetup />
-                      </ArtistRoute>
-                    }
-                  />
-                  <Route
-                    path="/find-artists"
-                    element={<FindArtists />}
-                  />
-                  
-                  {/* Admin Routes */}
-                  <Route
-                    path="/admin/dashboard"
-                    element={
-                      <AdminRoute>
-                        <AdminDashboard />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/users"
-                    element={
-                      <AdminRoute>
-                        <UserManager />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/products"
-                    element={
-                      <AdminRoute>
-                        <ProductManager />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/blog"
-                    element={
-                      <AdminRoute>
-                        <BlogManager />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/analytics"
-                    element={
-                      <AdminRoute>
-                        <AnalyticsDashboard />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/notifications"
-                    element={
-                      <AdminRoute>
-                        <NotificationCenter />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/payments"
-                    element={
-                      <AdminRoute>
-                        <PaymentSettings />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/shipping"
-                    element={
-                      <AdminRoute>
-                        <ShippingManager />
-                      </AdminRoute>
-                    }
-                  />
-                  <Route
-                    path="/admin/api"
-                    element={
-                      <AdminRoute>
-                        <APISettings />
-                      </AdminRoute>
-                    }
-                  />
-                </Routes>
-              </main>
+                    } />
+                    <Route path="/orders" element={
+                      <ProtectedRoute>
+                        <Orders />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/settings" element={
+                      <ProtectedRoute>
+                        <UserSettings />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/messages" element={
+                      <ProtectedRoute>
+                        <UserMessages />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/gallery" element={
+                      <ProtectedRoute>
+                        <UserGallery />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/bookings" element={
+                      <ProtectedRoute>
+                        <UserBookings />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/privacy" element={<PrivacyPolicy />} />
+                    <Route path="/terms" element={<TermsOfService />} />
+                    <Route path="/refunds" element={<RefundPolicy />} />
+                    <Route path="/admin" element={<AdminLogin />} />
+                    <Route
+                      path="/dashboard"
+                      element={
+                        <ProtectedRoute>
+                          <UserDashboard />
+                        </ProtectedRoute>
+                      }
+                    />
+                    <Route
+                      path="/artist/dashboard"
+                      element={
+                        <ArtistRoute>
+                          <ArtistDashboard />
+                        </ArtistRoute>
+                      }
+                    />
+                    <Route
+                      path="/artist/settings"
+                      element={
+                        <ArtistRoute>
+                          <ArtistSettings />
+                        </ArtistRoute>
+                      }
+                    />
+                    <Route
+                      path="/artist/setup"
+                      element={
+                        <ArtistRoute>
+                          <ArtistSetup />
+                        </ArtistRoute>
+                      }
+                    />
+                    <Route
+                      path="/find-artists"
+                      element={<FindArtists />}
+                    />
+                    
+                    {/* Admin Routes */}
+                    <Route
+                      path="/admin/dashboard"
+                      element={
+                        <AdminRoute>
+                          <AdminDashboard />
+                        </AdminRoute>
+                      }
+                    />
+                    <Route
+                      path="/admin/users"
+                      element={
+                        <AdminRoute>
+                          <UserManager />
+                        </AdminRoute>
+                      }
+                    />
+                    <Route
+                      path="/admin/products"
+                      element={
+                        <AdminRoute>
+                          <ProductManager />
+                        </AdminRoute>
+                      }
+                    />
+                    <Route
+                      path="/admin/blog"
+                      element={
+                        <AdminRoute>
+                          <BlogManager />
+                        </AdminRoute>
+                      }
+                    />
+                    <Route
+                      path="/admin/analytics"
+                      element={
+                        <AdminRoute>
+                          <AnalyticsDashboard />
+                        </AdminRoute>
+                      }
+                    />
+                    <Route
+                      path="/admin/notifications"
+                      element={
+                        <AdminRoute>
+                          <NotificationCenter />
+                        </AdminRoute>
+                      }
+                    />
+                    <Route
+                      path="/admin/payments"
+                      element={
+                        <AdminRoute>
+                          <PaymentSettings />
+                        </AdminRoute>
+                      }
+                    />
+                    <Route
+                      path="/admin/shipping"
+                      element={
+                        <AdminRoute>
+                          <ShippingManager />
+                        </AdminRoute>
+                      }
+                    />
+                    <Route
+                      path="/admin/api"
+                      element={
+                        <AdminRoute>
+                          <APISettings />
+                        </AdminRoute>
+                      }
+                    />
+                  </Routes>
+                </main>
 
-              <Footer />
-            </div>
+                <Footer />
+              </div>
 
-            <LoginModal 
-              isOpen={isLoginModalOpen}
-              onClose={() => setIsLoginModalOpen(false)}
-              onSwitchToSignUp={handleSwitchToSignUp}
-              onResetPassword={handleResetPassword}
-            />
-            <SignUpModal
-              isOpen={isSignUpModalOpen}
-              onClose={() => setIsSignUpModalOpen(false)}
-              onSwitchToLogin={handleSwitchToLogin}
-            />
-            <ResetPasswordModal
-              isOpen={isResetPasswordModalOpen}
-              onClose={() => setIsResetPasswordModalOpen(false)}
-              onSwitchToLogin={() => {
-                setIsResetPasswordModalOpen(false);
-                setIsLoginModalOpen(true);
-              }}
-            />
-
-          </MessagingProvider>
-        </BookingProvider>
-      </GalleryProvider>
-    </AuthProvider>
+              <LoginModal 
+                isOpen={isLoginModalOpen}
+                onClose={() => setIsLoginModalOpen(false)}
+                onSwitchToSignUp={handleSwitchToSignUp}
+                onResetPassword={handleResetPassword}
+              />
+              <SignUpModal
+                isOpen={isSignUpModalOpen}
+                onClose={() => setIsSignUpModalOpen(false)}
+                onSwitchToLogin={handleSwitchToLogin}
+              />
+              <ResetPasswordModal
+                isOpen={isResetPasswordModalOpen}
+                onClose={() => setIsResetPasswordModalOpen(false)}
+                onSwitchToLogin={() => {
+                  setIsResetPasswordModalOpen(false);
+                  setIsLoginModalOpen(true);
+                }}
+              />
+              <ToastContainer />
+            </MessagingProvider>
+          </BookingProvider>
+        </GalleryProvider>
+      </AuthProvider>
+    </Router>
   );
 }
